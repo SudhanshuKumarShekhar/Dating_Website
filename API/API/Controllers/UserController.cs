6663f016ede077f69b2a48c28cf439fbea575ed0
unknown <shekhar2119@gmail.com>
2023-03-10 10:40:13 +0530

    initial commit

---
+++ API/API/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using API.Data;
+using API.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserDbContext _context;
+
+        public UserController(UserDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/UserDetails
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUserDetails()
+        {
+            return await _context.Users.ToListAsync();
+        }
+    }
+}
---
+++ API/API/Data/UserDbContext.cs
@@ -0,0 +1,13 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
